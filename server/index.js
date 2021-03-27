15afd4d874e792904c898cb233b150b5b0ee4f09
Peter Phenow <peter.phenow@gmail.com>
2021-03-27 13:33:26 -0500

    connected to mongo database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/memories', { useNewUrlParser: true, useUnifiedTopology: true })
+  //if successfully connected...
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}.`)))
+  //if connection fails...
+  .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
