6ef64102be2381cd65c35039cc9ea5d64d25c502
Peter Phenow <peter.phenow@gmail.com>
2021-03-29 22:24:13 -0500

    created basic post, posts, and form components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.jpg';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Memories
+        </Typography>
+        {/* Image by <a href="https://pixabay.com/users/alexandra_koch-621802/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3130379">Alexandra_Koch</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3130379">Pixabay</a> */}
+        <img src={memories} alt='memories' height='60' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
