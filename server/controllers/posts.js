eb88014dbf51eb9a1eb1f87cf26b4f7d7711aa88
Peter Phenow <peter.phenow@gmail.com>
2021-03-29 21:25:25 -0500

    created mongoose model

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+//contains all the logic for the routes in the routes directory
+
+export const getPosts = (req, res) => {
+  res.send('THIS WORKS!');
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,10 @@
 import express from 'express';
+import { getPosts } from '../controllers/posts.js';
 
 const router = express.Router();
 
 //all routes in here are accessed by localhost:5000/posts because that was defined in index.js
 //route will be reached at localhost:5000/posts
-
-router.get('/', (req, res) => {
-  res.send('THIS works!');
-});
+router.get('/', getPosts);
 
 export default router;
