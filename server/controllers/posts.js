54f8dd857504fcbfbe7eab10bb884c49eb8d7c3f
Peter Phenow <peter.phenow@gmail.com>
2021-03-29 21:43:35 -0500

    logic to create and retrieve posts completed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,28 @@
 //contains all the logic for the routes in the routes directory
+import PostMessage from '../models/postMessage.js';
 
-export const getPosts = (req, res) => {
-  res.send('THIS WORKS!');
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    console.log(postMessages);
+
+    //will respond with an array of all messages
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  res.status(201).json(newPost);
+  try {
+    await newPost.save();
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 
 const router = express.Router();
 
 //all routes in here are accessed by localhost:5000/posts because that was defined in index.js
 //route will be reached at localhost:5000/posts
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
