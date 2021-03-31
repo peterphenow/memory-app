d873878730772715e4b383e560cda917d49e8794
Peter Phenow <peter.phenow@gmail.com>
2021-03-31 12:29:30 -0500

    set up styling for the components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,21 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.jpg';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxwidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        {/* Image by <a href="https://pixabay.com/users/alexandra_koch-621802/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3130379">Alexandra_Koch</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3130379">Pixabay</a> */}
+        <img className={classes.image} src={memories} alt='memories' height='60' />
+        <Typography className={classes.heading} variant='h2' align='center'>
           Memories
         </Typography>
         {/* Image by <a href="https://pixabay.com/users/alexandra_koch-621802/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3130379">Alexandra_Koch</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3130379">Pixabay</a> */}
-        <img src={memories} alt='memories' height='60' />
+        <img className={classes.image} src={memories} alt='memories' height='60' />
       </AppBar>
       <Grow in>
         <Container>
