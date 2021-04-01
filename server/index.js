cdb7ef2430eefce7094f70498a215a2dd5e75bd3
Peter Phenow <peter.phenow@gmail.com>
2021-03-31 21:00:38 -0500

    needed to move postRoutes below cors setup

--- server/index.js
+++ server/index.js
@@ -9,11 +9,12 @@ const app = express();
 dotenv.config();
 
 //every route inside postRoutes will start with /posts
-app.use('/posts', postRoutes);
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
