a056ffd25034306c50e4b402f1acedaeb3a205d1
Peter Phenow <peter.phenow@gmail.com>
2021-04-07 11:39:06 -0500

    like functionality added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -42,7 +42,6 @@ export const updatePost = async (req, res) => {
 };
 
 export const deletePost = async (req, res) => {
-  //renaming id to _id
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id.');
@@ -52,4 +51,15 @@ export const deletePost = async (req, res) => {
   console.log('DELETE');
 
   res.json({ message: 'Post deleted successfully.' });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id.');
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+  res.json(updatedPost);
 };
