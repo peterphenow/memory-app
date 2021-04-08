9661a046ce3f222ac8b50856b8320b54699f4e5b
Peter Phenow <peter.phenow@gmail.com>
2021-04-08 13:20:30 -0500

    created an example .env file for users to see

--- server/index.js
+++ server/index.js
@@ -8,12 +8,11 @@ import postRoutes from './routes/posts.js';
 const app = express();
 dotenv.config();
 
-//every route inside postRoutes will start with /posts
-
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+//every route inside postRoutes will start with /posts
 app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
