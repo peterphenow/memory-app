5405ea7ccc6161e43cf0bde06d2afb2c8ee1d749
Peter Phenow <peter.phenow@gmail.com>
2021-04-23 21:14:37 -0500

    signing up and in is working

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -41,13 +41,13 @@ const Navbar = () => {
         <img className={classes.image} src={memories} alt='memories' height='60' />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
-              {user.result.name.charAt(0)}
+            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>
+              {user?.result.name.charAt(0)}
             </Avatar>
             <Typography className={classes.userName} variant='h6'>
-              {user.result.name}
+              {user?.result.name}
             </Typography>
             <Button variant='contained' className={classes.logout} color='secondary' onClick={logout}>
               Logout
