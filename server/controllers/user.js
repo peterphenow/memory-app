1215b07693987ac868dd72e502f4ca079e787d99
Peter Phenow <peter.phenow@gmail.com>
2021-04-23 21:14:09 -0500

    fixing a typo in signin

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -19,7 +19,7 @@ export const signin = async (req, res) => {
 
     const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '1h' });
 
-    res.status(200).json({ message: existingUser, token });
+    res.status(200).json({ result: existingUser, token });
   } catch (error) {
     res.status(500).json({ message: 'Something went wrong!' });
   }
