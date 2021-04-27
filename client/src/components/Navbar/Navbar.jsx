e531a513d03a31e9d8379c8b08d81bb8a686b344
Peter Phenow <peter.phenow@gmail.com>
2021-04-26 21:55:50 -0500

    check if token has expired

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Typography, Avatar, Toolbar, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
 import useStyles from './styles';
 import memories from '../../images/memories.jpg';
@@ -24,7 +25,11 @@ const Navbar = () => {
   useEffect(() => {
     const token = user?.token;
 
-    // JWT...
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
